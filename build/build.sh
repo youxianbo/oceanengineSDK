ac5f69a204ab78fc12d1f38a3bebfe4a438cf740
joyber <joyber@qq.com>
2023-04-20 11:06:04 +0800

    增加巨量广告【获取自定义报表可用指标和维度】和【自定义报表】

--- build/build.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-openapi-generator-cli generate -g php -i api.json -c ./config.json -o ../  --skip-validate-spec --enable-post-process-file
--- build/push.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-cd ../
-./git_push.sh youxianbo oceanengineSDK $1
